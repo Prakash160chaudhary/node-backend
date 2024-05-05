11daa168261888ddb55343cfd1910ff56fccb899
hellopasal <hellopasalshop@gmail.com>
2024-05-05 16:16:24 +0545

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -22,7 +22,7 @@ const registerUser = asyncHandler( async (req, res) => {
   // return res
 
   const { fullname, email, username, password }= req.body
-  console.log("email: ", email);
+  // console.log("email: ", email);
 
   // if(fullname === "") {
   //   throw new ApiError(400, "fullname is reuired")
@@ -34,7 +34,7 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new ApiError(400, "All firlds are required")
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username },{ email }]
   })
 
@@ -42,8 +42,16 @@ const registerUser = asyncHandler( async (req, res) => {
     throw new ApiError(409, "User with email or username already exist")
   }
 
+  // test
+  // console.log(req.files);
+
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is reuired")
@@ -62,7 +70,7 @@ const registerUser = asyncHandler( async (req, res) => {
     coverImage: coverImage?.url || "",
     email,
     password,
-    username: username.toLowercase()
+    username: username.toLowerCase()
   })
 
   const createdUser = await User.findById(user._id).select(
